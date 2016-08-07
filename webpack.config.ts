41d42d07d58313525218f4036327c47b599d1965
PatrickJS <github@gdi2290.com>
2016-08-07 11:50:36 -0700

    feat: ngc with ngfactory

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,7 +1,7 @@
 import 'core-js/es6';
 import 'core-js/es7/reflect';
 import 'ts-helpers';
-// needed to create context for resolveNgRoute
+// needed to create context for angular2-webpack and resolveNgRoute
 /**
  * @author: @AngularClass
  */
@@ -10,16 +10,18 @@ const {
   HotModuleReplacementPlugin,
   DefinePlugin,
   ProgressPlugin,
+  NormalModuleReplacementPlugin,
 
   optimize: {
     CommonsChunkPlugin,
-    DedupePlugin
+    DedupePlugin,
+    UglifyJsPlugin
   }
 
 } = require('webpack');
 const {ForkCheckerPlugin} = require('awesome-typescript-loader');
-const resolveNgRoute = require('@angularclass/resolve-angular-routes')
-
+const ng2webpack = require('angular2-webpack');
+const CompressionPlugin = require('compression-webpack-plugin');
 const path = require('path');
 
 function root(__path = '.') {
@@ -30,10 +32,10 @@ function root(__path = '.') {
 function webpackConfig(options: EnvOptions = {}): WebpackConfig {
 
   const CONSTANTS = {
-    ENV: JSON.stringify(options.ENV),
-    HMR: options.HMR,
-    PORT: 3000,
-    HOST: 'localhost'
+    'ENV': JSON.stringify(options.ENV),
+    'HMR': options.HMR,
+    'PORT': 3000,
+    'HOST': 'localhost'
   };
 
   return {
@@ -46,7 +48,10 @@ function webpackConfig(options: EnvOptions = {}): WebpackConfig {
     entry: {
       polyfills: './src/polyfills.browser',
       vendor:    './src/vendor.browser',
-      main:      './src/main.browser'
+      // main:      './src/main.browser'
+      // polyfills: './ngfactory/src/polyfills.browser',
+      // vendor:    './ngfactory/src/vendor.browser',
+      main:      './ngfactory/src/main.browser.ngfactory'
     },
 
     output: {
@@ -57,19 +62,7 @@ function webpackConfig(options: EnvOptions = {}): WebpackConfig {
     },
 
     module: {
-      preLoaders: [
-        // fix angular2
-        {
-          test: /(systemjs_component_resolver|system_js_ng_module_factory_loader)\.js$/,
-          loader: 'string-replace-loader',
-          query: {
-            search: '(lang_1(.*[\\n\\r]\\s*\\.|\\.))?(global(.*[\\n\\r]\\s*\\.|\\.))?(System|SystemJS)(.*[\\n\\r]\\s*\\.|\\.)import',
-            replace: 'System.import',
-            flags: 'g'
-          }
-        }
-        // end angular2 fix
-      ],
+      preLoaders: [ ng2webpack.loaders() ], // fix angular2
 
       loaders: [
         // Support for .ts files.
@@ -91,22 +84,113 @@ function webpackConfig(options: EnvOptions = {}): WebpackConfig {
 
     plugins: [
       // fix angular2
-      new ContextReplacementPlugin(
-        /angular\/core\/src\/linker/,
-        root('./src'),
-        resolveNgRoute(root('./src'))
-      ),
+      ng2webpack.plugins(root('./src')),
       // end angular2 fix
 
-      new HotModuleReplacementPlugin(),
+      // new HotModuleReplacementPlugin(),
       new ForkCheckerPlugin(),
       new CommonsChunkPlugin({ name: ['main', 'vendor', 'polyfills'], minChunks: Infinity }),
       new DefinePlugin(CONSTANTS),
-      new ProgressPlugin({})
+      new ProgressPlugin({}),
+
+      // new NormalModuleReplacementPlugin(
+      //   /facade\/async/,
+      //   root('node_modules/@angular/core/esm/src/facade/async.js')
+      // ),
+      // new NormalModuleReplacementPlugin(
+      //   /facade\/lang/,
+      //   root('node_modules/@angular/core/esm/src/facade/lang.js')
+      // ),
+      // new NormalModuleReplacementPlugin(
+      //   /facade\/base_wrapped_exception/,
+      //   root('node_modules/@angular/core/esm/src/facade/base_wrapped_exception.js')
+      // ),
+      // new NormalModuleReplacementPlugin(
+      //   /facade\/collection/,
+      //   root('node_modules/@angular/core/esm/src/facade/collection.js')
+      // ),
+      // new NormalModuleReplacementPlugin(
+      //   /facade\/exception_handler/,
+      //   root('node_modules/@angular/core/esm/src/facade/exception_handler.js')
+      // ),
+      // new NormalModuleReplacementPlugin(
+      //   /facade\/exceptions/,
+      //   root('node_modules/@angular/core/esm/src/facade/exceptions.js')
+      // ),
+      // new NormalModuleReplacementPlugin(
+      //   /facade\/math/,
+      //   root('node_modules/@angular/core/esm/src/facade/math.js')
+      // ),
+      // new NormalModuleReplacementPlugin(
+      //   /facade\/promise/,
+      //   root('node_modules/@angular/core/esm/src/facade/promise.js')
+      // ),
+
+
+      new UglifyJsPlugin({
+        // beautify: true,
+        // comments: true,
+        // mangle: false,
+        //
+      //
+        beautify: false,
+        mangle: { screw_ie8 : true },
+        compress: { screw_ie8: true },
+        comments: false
+      }),
+
+      new CompressionPlugin({
+        algorithm: "gzip",
+        test: /\.js$|\.html$/,
+        threshold: 10240,
+        minRatio: 0.8
+      }),
+      new NormalModuleReplacementPlugin(
+        /facade\/async/,
+        root('node_modules/@angular/core/src/facade/async.js')
+      ),
+      new NormalModuleReplacementPlugin(
+        /facade\/lang/,
+        root('node_modules/@angular/core/src/facade/lang.js')
+      ),
+      new NormalModuleReplacementPlugin(
+        /facade\/base_wrapped_exception/,
+        root('node_modules/@angular/core/src/facade/base_wrapped_exception.js')
+      ),
+      new NormalModuleReplacementPlugin(
+        /facade\/collection/,
+        root('node_modules/@angular/core/src/facade/collection.js')
+      ),
+      new NormalModuleReplacementPlugin(
+        /facade\/exception_handler/,
+        root('node_modules/@angular/core/src/facade/exception_handler.js')
+      ),
+      new NormalModuleReplacementPlugin(
+        /facade\/exceptions/,
+        root('node_modules/@angular/core/src/facade/exceptions.js')
+      ),
+      new NormalModuleReplacementPlugin(
+        /facade\/math/,
+        root('node_modules/@angular/core/src/facade/math.js')
+      ),
+      new NormalModuleReplacementPlugin(
+        /facade\/promise/,
+        root('node_modules/@angular/core/esm/src/facade/promise.js')
+      ),
     ],
 
     resolve: {
       extensions: ['', '.ts', '.js', '.json'],
+      alias: {
+        // 'rxjs': root('node_modules/rxjs-es'),
+        // '@angular/core/src': root('node_modules/@angular/core/esm/src'),
+        // '@angular/common/src': root('node_modules/@angular/common/esm/src'),
+        // '@angular/forms/src': root('node_modules/@angular/forms/esm/src'),
+        // '@angular/http/src': root('node_modules/@angular/http/esm/src'),
+        // '@angular/router/src': root('node_modules/@angular/router/esm/src'),
+        // '@angular/compiler/src': root('node_modules/@angular/cpmiler/esm/src'),
+        // '@angular/platform-browser/src': root('node_modules/@angular/platform-browser/esm/src')
+      }
     },
 
     devServer: {
@@ -122,11 +206,9 @@ function webpackConfig(options: EnvOptions = {}): WebpackConfig {
       process: true,
       Buffer: false,
       crypto: 'empty',
-      module: false,
-      clearImmediate: false,
-      setImmediate: false,
-      clearTimeout: true,
-      setTimeout: true
+      net: 'empty',
+      tls: 'empty',
+      dns: 'empty'
     }
   };
 }
@@ -160,6 +242,7 @@ interface WebpackConfig {
   resolve?: {
     root?: string;
     extensions?: Array<string>;
+    alias?: any
   };
   devServer?: {
     contentBase?: string;
@@ -177,6 +260,9 @@ interface WebpackConfig {
     clearImmediate?: boolean;
     setImmediate?: boolean
     clearTimeout?: boolean;
-    setTimeout?: boolean
+    setTimeout?: boolean;
+    net: string | boolean;
+    tls: string | boolean;
+    dns: string | boolean;
   };
 }
